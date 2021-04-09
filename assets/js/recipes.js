34e37ac51ddf1cba61e1cd667abc2b7883c3fc34
dejagraver <dejaamaartin@gmail.com>
2021-04-09 19:08:49 -0400

    recipes.js attempting to add classes using JS

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -29,7 +29,7 @@ function getRecipeData() {
 //display random recipes in an array
 function displayRecipe(data) {
   var Recipe = data.meals[0];
-  var RecipeDiv = document.getElementById("recipe-list"); 
+  var RecipeDiv = document.getElementById("recipe-list"); ``
 
   var RecipeImg = document.createElement("img");
   RecipeImg.src = Recipe.strMealThumb;
